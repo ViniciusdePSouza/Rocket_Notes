e76901d328e266e5c620b782b6607ec093bd48a8
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-09-07 15:39:31 -0300

    Routes added

---
+++ rocketnotes/src/pages/NewNote/index.jsx
@@ -0,0 +1,45 @@
+import { Container, Form } from './styles'
+
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { TextArea } from '../../components/TextArea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+
+export function NewNote() {
+    return (
+        <Container>
+            <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>New Note</h1>
+
+                        <a href="/">Back</a>
+                    </header>
+
+                    <Input
+                        placeholder="Title" />
+
+                    <TextArea placeholder="Obs:" />
+
+                    <Section title="Links utils">
+                        <NoteItem value="https://github.com/ViniciusdePSouza" />
+                        <NoteItem isNew placeholder="New link" />
+                    </Section>
+
+                    <Section title="Markers">
+                        <div className="tags">
+                            <NoteItem value="ReactJS" />
+                            <NoteItem isNew placeholder="New tag" />
+                        </div>
+                    </Section>
+
+                    <Button title="Save" type="submit"/>
+                </Form>
+            </main>
+        </Container>
+    )
+}
---
+++ rocketnotes/src/routes/app.routes.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { NewNote } from '../pages/NewNote'
+import { Home } from '../pages/Home'
+import { Details } from '../pages/Details'
+import { Profile } from '../pages/Profile'
+
+export function AppRoutes(){
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/newnote" element={<NewNote/>} />
+        <Route path="/details/:id" element={<Details/>} />
+        <Route path="/profile" element={<Profile/>} />
+    </Routes>
+}
