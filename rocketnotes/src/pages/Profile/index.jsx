9bbd1b28d0c3916b0eb92d3c714ebdfe82db4203
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-09-07 13:01:07 -0300

    Profile interface done

---
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -0,0 +1,60 @@
+import { Container, Form, Avatar } from './styles'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { FiArrowLeft, FiMail, FiUser, FiLock, FiCamera } from 'react-icons/fi'
+
+export function Profile() {
+    return (
+        <Container>
+
+            <header>
+                <a href="">
+                    <FiArrowLeft />
+                </a>
+            </header>
+
+            <Form>
+                <Avatar>
+                    <img src="https://github.com/ViniciusdePSouza.png" alt="User Profile Photo" />
+
+                    <label htmlFor="Avatar">
+
+                        <FiCamera />
+
+                        <input id="avatar" type="file" />
+                    </label>
+                </Avatar>
+
+
+                <Input
+                    placeholder="Name"
+                    type="text"
+                    icon={FiUser}
+                />
+
+                <Input
+                    placeholder="E-mail"
+                    type="email"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Current Password"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Input
+                    placeholder="New Password"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Save Changes" />
+
+            </Form>
+
+        </Container>
+    )
+}
